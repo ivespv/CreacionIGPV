872b4c57f2458a088fcbc2165d3f713ef9a880bc
ivespv <ivespv3@gmail.com>
2024-12-06 17:26:47 -0400

    Proyecto Final

--- Practicas/Proyecto Final/codigo.js
+++ Practicas/Proyecto Final/codigo.js
@@ -1,1 +1,30 @@
+const baseDeDatos = [
+    {
+        id: 1,
+        nombre: 'FORTIGATE 100F',
+        precio: 15000,
+        imagen: 'Imagenes/FortiGate 100F.jpg',
+        imagen1: 'Imagenes/fotigate.webp'
+    }
+    {
+        id: 2,
+        nombre: 'Router Cisco 4451-X',
+        precio: 49000,
+        imagen: 'Imagenes/R1.jpg',
+        imagen1: 'Imagenes/R2.jpg',
+    },
+    {
+        id: 3,
+        nombre: 'Switches Cisco 9300',
+        precio: 27000,
+        imagen: 'Imagenes/s1.jpg',
+        imagen: 'Imagenes/s2.jpg',
+    },
+    {
+        id: 4,
+        nombre: 'Fresas',
+        precio: 0.6,
+        imagen: 'fresas.jpg'
+    }
 
+];
